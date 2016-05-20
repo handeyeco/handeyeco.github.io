7dd57d5906f5acd1d7d2470c65165a62429dd464
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-05-20 14:17:27 -0700

    Add smooth scroll and update nav stick

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,39 @@
 $(document).ready(function() {
-	//Stick nav bar
-	var navBar = $("#navigation"),
-		 navClass = "navigation-stick",
-		 pos = navBar.position();      
+	var $win = $(window);
 
-	$(window).scroll(function() {
-			var windowpos = $(window).scrollTop();
-			if (windowpos >= pos.top) {
-    			navBar.addClass(navClass);
-			} else {
-    			navBar.removeClass(navClass); 
-			}
+	//Stick navigation
+	$win.scroll(function() {
+		if ($win.scrollTop() >= $win.height()) {
+			$("nav").addClass("navigation-stick");
+		} else {
+			$("nav").removeClass("navigation-stick"); 
+		}
 	});
-	//End stick nav bar
+
+	//Slide nav links to anchor
+	$("nav a[href^='#']").click(function (e) {
+    e.preventDefault();
+
+    $("html, body").stop().animate({
+      "scrollTop": $(this.hash).offset().top
+    }, 900, "swing", function () {
+      window.location.hash = this.hash;
+    });
+	});
+
+	//Show job details to mobile
+	//Needs to be adjusted so accordian only works for mobile
+	/*$(".job-listing").click(function () {
+		if ($(this).hasClass("active")) {
+			$(this).find(".hide-mobile").slideToggle(500);
+			$(this).removeClass("active");
+		} else {
+			$(".job-listing").removeClass("active");
+			$(".job-listing .hide-mobile").slideUp(500);
+			$(this).find(".hide-mobile").slideToggle(500);
+			$(this).addClass("active");
+		}
+	});*/
+
+
 });
