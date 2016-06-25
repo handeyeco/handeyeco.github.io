691c2da0647bc6fdb09a43c55283d8298b5f1631
Matthew Curtis <matthew@matthewbryancurtis.com>
2016-06-24 17:58:38 -0700

    Prepare to merge Bootstrap branch

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
-	var $win = $(window);
+    var $win = $(window);
 
-	//Stick navigation
-	$win.scroll(function() {
-		if ($win.scrollTop() >= $win.height()) {
-			$("nav").addClass("navigation-stick");
-		} else {
-			$("nav").removeClass("navigation-stick"); 
-		}
-	});
+    //Stick navigation
+    $win.scroll(function() {
+        if ($win.scrollTop() >= $win.height()) {
+            $("nav").addClass("navigation-stick");
+        } else {
+            $("nav").removeClass("navigation-stick");
+        }
+    });
 
-	//Slide nav links to anchor
-	$("nav a[href^='#']").click(function (e) {
-    e.preventDefault();
+    //Slide nav links to anchor
+    $("nav a[href^='#']").click(function(e) {
+        e.preventDefault();
 
-    $("html, body").stop().animate({
-      "scrollTop": $(this.hash).offset().top
-    }, 900, "swing");
-	});
+        $("html, body").stop().animate({
+            "scrollTop": $(this.hash).offset().top
+        }, 900, "swing");
+    });
+
+    //Hide nav on click
+    $('.nav a').on('click', function() {
+        $('.navbar-toggle').click();
+    });
 
 });
